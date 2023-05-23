72d57eef0825cedafa75e80a92ca2c6b88438a85
zhvarikk <smisha3000@gmail.com>
2023-05-23 21:30:43 +0200

    backend(feat): build backend structure and connect backend to db

---
+++ GamificationToIpBackend/GamificationToIP/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace GamificationToIP.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
---
+++ GamificationToIpBackend/GamificationToIP/Models/Student.cs
@@ -0,0 +1,18 @@
+﻿
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace BulkyBookWeb.Models;
+public class Student
+{
+    [Key]
+    public int Id { get; set; }
+    [Required]
+    public string FirstName { get; set; } = null!;
+
+    public string MiddleName { get; set; } = null!;
+
+    public string LastName { get; set; } = null!;
+
+    public string Email { get; set; } = null!;
+}
