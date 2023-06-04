d6fe5c3a2bdb0366c4581985459d24bb98634f40
zhvarikk <smisha3000@gmail.com>
2023-06-04 09:55:58 +0200

    backend(feat): implement api to getGeneratedTests

--- GamificationAPI/GamificationAPI/Controllers/GeneratedTestsController.cs
+++ GamificationAPI/GamificationAPI/Controllers/GeneratedTestsController.cs
@@ -1,17 +1,22 @@
 ﻿using BulkyBookWeb.Models;
 using GamificationAPI.Interfaces;
 using GamificationAPI.Models;
+using GamificationToIP.Context;
+using GamificationToIP.Models;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using Newtonsoft.Json;
 
 [Route("api/generatedTests")]
 [ApiController]
 public class GeneratedTestController : ControllerBase
 {
     private readonly IGeneratedTests _generatedTestService;
-
-    public GeneratedTestController(IGeneratedTests generatedTestService)
+    private readonly ApplicationDbContext _dbContext;
+    public GeneratedTestController(IGeneratedTests generatedTestService, ApplicationDbContext dbContext)
     {
         _generatedTestService = generatedTestService;
+        _dbContext = dbContext; 
     }
 
     // GET: api/scoreboard
@@ -38,6 +43,58 @@ public class GeneratedTestController : ControllerBase
         var generatedTest = await _generatedTestService.GenerateTest(studentId,testId,numberOfQuestions);
         return Ok("Test was generated");
     }
+
+    [HttpGet("{studentId}/{testId}")]
+    public async Task<ActionResult<GeneratedTestDto>> GetGeneratedTest(int studentId, int testId)
+    {
+        var generatedTest = await _dbContext.GeneratedTest
+        .Include(gt => gt.Test)
+        .Include(gt => gt.Test.Questions)
+        .ThenInclude(q => q.Answers)
+        .FirstOrDefaultAsync(gt => gt.StudentId == studentId && gt.TestId == testId);
+
+        if (generatedTest == null)
+        {
+            return NotFound();
+        }
+
+        var studentQuestions = await _dbContext.StudentQuestions
+            .Include(sq => sq.Question)
+            .Where(sq => sq.GeneratedTestId == generatedTest.Id)
+            .OrderBy(sq => sq.Id)
+            .ToListAsync();
+
+        var generatedTestDto = new GeneratedTestDto
+        {
+            Id = generatedTest.Test.Id,
+            Title = generatedTest.Test.Title,
+            ImageUrl = generatedTest.Test.ImageUrl,
+            Description = generatedTest.Test.Description,
+            TimeSeconds = generatedTest.Test.TimeSeconds,
+            Questions = studentQuestions.Select(sq => new GeneratedQuestionDto
+            {
+                Id = sq.Question.Id,
+                QuestionText = sq.Question.QuestionText,
+                CorrectAnswer = sq.Question.CorrectAnswer,
+                SelectedAnswer = sq.Question.SelectedAnswer,
+                Answers = sq.Question.Answers.Select(a => new GeneratedAnswerDto
+                {
+                    Id = a.Id,
+                    Identifier = a.Identifier,
+                    AnswerText = a.AnswerText
+                }).ToList()
+            }).ToList()
+        };
+
+        var jsonSettings = new JsonSerializerSettings
+        {
+            ReferenceLoopHandling = ReferenceLoopHandling.Ignore
+        };
+
+        var json = JsonConvert.SerializeObject(generatedTestDto, Formatting.None, jsonSettings);
+
+        return Content(json, "application/json");
+    }
 }
 
 public class GenerateTestRequest
@@ -45,4 +102,30 @@ public class GenerateTestRequest
     public int TestId { get; set; }
     public int StudentId { get; set; }
     public int NumberOfQuestions { get; set; }
+}
+
+public class GeneratedTestDto
+{
+    public int Id { get; set; }
+    public string Title { get; set; }
+    public string ImageUrl { get; set; }
+    public string Description { get; set; }
+    public int TimeSeconds { get; set; }
+    public List<GeneratedQuestionDto> Questions { get; set; }
+}
+
+public class GeneratedQuestionDto
+{
+    public int Id { get; set; }
+    public string QuestionText { get; set; }
+    public string CorrectAnswer { get; set; }
+    public string SelectedAnswer { get; set; }
+    public List<GeneratedAnswerDto> Answers { get; set; }
+}
+
+public class GeneratedAnswerDto
+{
+    public int Id { get; set; }
+    public string Identifier { get; set; }
+    public string AnswerText { get; set; }
 }
