6a77c443a497daff1d5cae9d3b79291bfd9b8bda
Stephen Nedd <stephennedd1@gmail.com>
2023-06-06 17:22:52 +0200

    update sn

--- GamificationAPI/GamificationAPI/Data/Migrations/20230605115713_SecondMigration.Designer.cs
+++ GamificationAPI/GamificationAPI/Data/Migrations/20230605115713_SecondMigration.Designer.cs
@@ -12,7 +12,7 @@ using Npgsql.EntityFrameworkCore.PostgreSQL.Metadata;
 namespace GamificationAPI.Data.Migrations
 {
     [DbContext(typeof(ApplicationDbContext))]
-    [Migration("20230604095516_SecondMigration")]
+    [Migration("20230605115713_SecondMigration")]
     partial class SecondMigration
     {
         /// <inheritdoc />
