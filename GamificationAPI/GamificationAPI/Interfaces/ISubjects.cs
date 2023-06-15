95528e94e62a9d05ee2261fb2c9839593413a825
zhvarikk <smisha3000@gmail.com>
2023-06-15 12:09:14 +0200

    backend(feat): create api to update quiz page in admin panel

--- GamificationAPI/GamificationAPI/Interfaces/ISubjects.cs
+++ GamificationAPI/GamificationAPI/Interfaces/ISubjects.cs
@@ -1,10 +1,14 @@
 ﻿
 
+using Microsoft.AspNetCore.Mvc;
+
 namespace GamificationAPI.Interfaces
 
 {
     public interface ISubjects
     {
         Task<List<Subject>> GetSubjects();
+        Task<RootObject> UpdateTables(RootObject data);
     }
+
 }
