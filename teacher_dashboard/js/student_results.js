cd79f2f554402541a92f7157972a495e00ece5b5
Stephen <stephennedd1@gmail.com>
2023-06-21 14:44:39 +0200

    updated student dashboard page with groups

--- teacher_dashboard/js/student_results.js
+++ teacher_dashboard/js/student_results.js
@@ -1,18 +1,18 @@
-const studentTableCheckbox = document.getElementById('all-students-checkbox');
 const tableHeader = document.getElementById('student-results-card-header');
-var students = [];
+const groupSelector = document.getElementById("group-selection-dropdown");
+//var students = [];
 
-studentTableCheckbox.addEventListener('change', function () {
-    if (studentTableCheckbox.checked) {
-        console.log('all-students-checked');
-        tableHeader.innerHTML = 'All Students';
-        getStudents(true);
-    } else {
-        console.log('all-students-not-checked');
-        tableHeader.innerHTML = 'My Students';
-        getStudents(false);
-    }
-});
+// studentTableCheckbox.addEventListener('change', function () {
+//     if (studentTableCheckbox.checked) {
+//         console.log('all-students-checked');
+//         tableHeader.innerHTML = 'All Students';
+//         getStudents(true);
+//     } else {
+//         console.log('all-students-not-checked');
+//         tableHeader.innerHTML = 'My Students';
+//         getStudents(false);
+//     }
+// });
 
 async function getStudents(allStudents) {
     students = [];
@@ -28,3 +28,28 @@ async function getStudents(allStudents) {
 
     return students;
 }
+
+
+$(document).ready(function () {
+    //var groups = [];
+    var groups = [{id: 1 ,name: "Group 1"}, {id: 2 ,name: "Group 2"}, {id: 3 ,name: "Group 3"}];
+    // TODO get the groups from the server
+
+    // Add the groups to the dropdown
+    groups.forEach((group) => {
+        var option = document.createElement("option");
+        option.text = group.name;
+        option.value = group.id;
+        console.log(group.name);
+        groupSelector.appendChild(option);
+
+    });
+    
+});
+
+groupSelector.addEventListener('change', function () {
+    console.log('group changed');
+    // TODO get the students from the selected group
+    // TODO update the students table
+}
+);
