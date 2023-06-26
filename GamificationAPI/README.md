d7aef26446aae86dd21cfd21b6241210498c4ad2
Stephen Nedd <stephennedd1@gmail.com>
2023-06-26 14:05:51 +0200

    readme updates and authentication check now works on frontend

--- GamificationAPI/README.md
+++ GamificationAPI/README.md
@@ -1,4 +1,11 @@
-# gamication_ip
+# API for GamificationToIP
+
+tech stack: .NET 6.0, C#, PostgreSQL, Entity Framework Core, Docker
+![Generic badge](https://img.shields.io/badge/.NET-6.0-green.svg)
+![Generic badge](https://img.shields.io/badge/C%23-9.0-blue.svg)
+![Generic badge](https://img.shields.io/badge/PostgreSQL-13.4-blue.svg)
+![Generic badge](https://img.shields.io/badge/Entity%20Framework%20Core-6.0.0-blue.svg)
+![Generic badge](https://img.shields.io/badge/Docker-20.10.8-blue.svg)
 
 ## How to run backend?
 
@@ -8,13 +15,21 @@
 
 3. Go to GamificationToIpBackend/appsettings.json and paste your password to postgres db
 
-4. Click on green triangle on top to run the backend
+4. run the project using the following command in terminal:
+
+```sh
+dotnet run
+```
 
 ## How to create a db migration which later be used to create/drop the tables
 
 1. Open up Package Manager Console at the bottom of your Microsoft Visual Studio 2022
 
-2. Run: Add-Migration SecondMigration -c ApplicationDbContext -o Data/Migrations
+2. Run the following command in the Package Manager console:
+
+```sh
+Add-Migration SecondMigration -c ApplicationDbContext -o Data/Migrations
+```
 
 ## How to create tables in db?
 
@@ -22,7 +37,11 @@
 
 2. Open up Package Manager Console at the bottom of your Microsoft Visual Studio 2022
 
-3. Run: Update-Database
+3. Run the following command in the Package Manager console:
+
+```sh
+Update-Database
+```
 
 ## How to drop tables in db?
 
@@ -30,11 +49,18 @@
 
 2. Open up Package Manager Console at the bottom of your Microsoft Visual Studio 2022
 
-3. Run: Drop-Database
+3. Run the following command in the Package Manager console:
+
+```sh
+Drop-Database
+```
 
 ## How to seed db with the data?
 
-1. Go to /GamificationToIpBackend/GamificationToIP in your terminal 
+1. Go to /GamificationToIpBackend/GamificationToIP in your terminal
 
-2. Run: dotnet run seeddata
+2. Run the following command in the terminal:
 
+```sh
+dotnet run seeddata
+```
--- teacher_dashboard/js/auth_check.js
+++ teacher_dashboard/js/auth_check.js
@@ -1,38 +1,56 @@
 // Function to check if user is authenticated and redirect after a delay
 window.addEventListener("DOMContentLoaded", function() {
-    var authenticationCheckComplete = false; // Flag to track authentication check completion
-  
-    // Function to redirect after a delay
-    function redirectTo(destination) {
-      if (authenticationCheckComplete) {
-        window.location.href = destination;
-      } else {
-        setTimeout(function() {
-          console.log("Redirecting to " + destination);
-          redirectTo(destination);
-        }, 500 * 6);
-      }
+    // Check if user is authenticated
+    if (isAuthenticated()) {
+      // User is authenticated, redirect to dashboard
+      console.log("User is authenticated");
+    } else {
+      // User is not authenticated, redirect to login
+      console.log("User is not authenticated");
+      this.window.location.href = "/teacher_dashboard/pages/login.html";
     }
-  
-    // Make a GET request to check the user's authentication status
-    fetch("/api/check-authentication")
-      .then(function(response) {
-        if (response.ok) {
-          // User is authenticated
-          authenticationCheckComplete = true;
-          console.log("User is authenticated");
-          redirectTo("/pages/admin-panel.html");
-        } else {
-          // User is not authenticated
-          authenticationCheckComplete = true;
-          console.log("User is not authenticated");
-          //redirectTo("http://127.0.0.1:5500/teacher_dashboard/pages/admin-panel.html");
-          redirectTo("http://127.0.0.1:5500/teacher_dashboard/pages/login.html");
-        }
+});
+
+  // Function to check if the JWT token is authenticated
+function isAuthenticated() {
+  // try and get the JWT token from the cookie
+  try {
+    var jwtToken = document.cookie.split(";").filter(function(item) {
+      return item.includes("jwt=");
+    })[0].split("=")[1];
+  } catch (error) {
+    console.log("Error getting JWT token from cookie/token not found");
+    return false;
+  }
+
+  // Check if the JWT token exists
+  if (!jwtToken) {
+    console.log("Token not found");
+    return false;
+  }
+
+  var decodedToken = parseJwt(jwtToken); // Parse the JWT token and extract the claims
+  var expirationDate = new Date(decodedToken.exp * 1000); // Extract the JWT token expiration date
+
+  // Check if the current date is past the JWT token expiration date
+  if (new Date() > expirationDate) {
+    return false;
+  } else {
+    return true;
+  }
+}
+
+function parseJwt(token) {
+  var base64Url = token.split('.')[1];
+  var base64 = base64Url.replace(/-/g, '+').replace(/_/g, '/');
+  var jsonPayload = decodeURIComponent(
+    atob(base64)
+      .split('')
+      .map(function (c) {
+        return '%' + ('00' + c.charCodeAt(0).toString(16)).slice(-2);
       })
-      .catch(function(error) {
-        // Handle any error that occurred during the authentication status check
-        console.error(error);
-        // Show an error message or take appropriate action
-      });
-  });
+      .join('')
+  );
+
+  return JSON.parse(jsonPayload);
+}
