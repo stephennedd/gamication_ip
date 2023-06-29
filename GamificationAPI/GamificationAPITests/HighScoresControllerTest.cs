57db14cf5346edb620479138e411a39af893ad2a
Wojciech Rusiecki <miecze123@onet.pl>
2023-06-29 21:51:10 +0200

    tests for most of controllers but there is an issue

--- GamificationAPI/GamificationAPITests/HighScoresControllerTest.cs
+++ GamificationAPI/GamificationAPITests/HighScoresControllerTest.cs
@@ -12,157 +12,159 @@ using GamificationToIP.Models;
 using Microsoft.IdentityModel.Tokens;
 using System.IdentityModel.Tokens.Jwt;
 using System.Text;
-
-public class HighScoresControllerTest
+namespace GamificationAPITests
 {
-    private HighScoresController _controller;
-    private Mock<ILeaderboards> _leaderboardService;
-    private Mock<IHighScores> _highScoreService;
-    private Mock<IUsers> _userService;
-    private ApplicationDbContext _context;
-
-    public HighScoresControllerTest()
+    public class HighScoresControllerTest
     {
-        // Setup DbContext
-        var options = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
-            .Options;
-        _context = new ApplicationDbContext(options);
-        _context.Database.EnsureCreated();
+        private HighScoresController _controller;
+        private Mock<ILeaderboards> _leaderboardService;
+        private Mock<IHighScores> _highScoreService;
+        private Mock<IUsers> _userService;
+        private ApplicationDbContext _context;
 
-        SeedDatabase();
-
-        // Setup Mock Services
-        _leaderboardService = new Mock<ILeaderboards>();
-        _highScoreService = new Mock<IHighScores>();
-        _userService = new Mock<IUsers>();
-
-        _controller = new HighScoresController(_leaderboardService.Object, _highScoreService.Object, _userService.Object);
-    }
-
-    private void SeedDatabase()
-    {
-        // Add mock data to the database here.
-        // Make sure to add a user with ID "ExistingUser"
-    }
-
-    private string GenerateMockToken(string userId)
-    {
-        var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(userId));
-        var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
-        var token = new JwtSecurityToken(
-            expires: DateTime.Now.AddMinutes(60),
-            signingCredentials: credentials
-        );
-
-        var tokenHandler = new JwtSecurityTokenHandler();
-        return tokenHandler.WriteToken(token);
-    }
-
-    private void SetupControllerUser(HighScoresController controller, string userId)
-    {
-        var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+        public HighScoresControllerTest()
         {
+            // Setup DbContext
+            var options = new DbContextOptionsBuilder<ApplicationDbContext>()
+                .UseInMemoryDatabase(databaseName: Guid.NewGuid().ToString())
+                .Options;
+            _context = new ApplicationDbContext(options);
+            _context.Database.EnsureCreated();
+
+            SeedDatabase();
+
+            // Setup Mock Services
+            _leaderboardService = new Mock<ILeaderboards>();
+            _highScoreService = new Mock<IHighScores>();
+            _userService = new Mock<IUsers>();
+
+            _controller = new HighScoresController(_leaderboardService.Object, _highScoreService.Object, _userService.Object);
+        }
+
+        private void SeedDatabase()
+        {
+            // Add mock data to the database here.
+            // Make sure to add a user with ID "ExistingUser"
+        }
+
+        private string GenerateMockToken(string userId)
+        {
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(userId));
+            var credentials = new SigningCredentials(key, SecurityAlgorithms.HmacSha512Signature);
+            var token = new JwtSecurityToken(
+                expires: DateTime.Now.AddMinutes(60),
+                signingCredentials: credentials
+            );
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            return tokenHandler.WriteToken(token);
+        }
+
+        private void SetupControllerUser(HighScoresController controller, string userId)
+        {
+            var user = new ClaimsPrincipal(new ClaimsIdentity(new Claim[]
+            {
             new Claim(ClaimTypes.Name, userId),
             new Claim("Authorization", GenerateMockToken(userId)) // Add the token as a claim
-        }));
+            }));
 
-        var httpContext = new DefaultHttpContext() { User = user };
-        httpContext.Request.Headers["Authorization"] = "Bearer " + GenerateMockToken(userId); // Add the token as a Bearer token
+            var httpContext = new DefaultHttpContext() { User = user };
+            httpContext.Request.Headers["Authorization"] = "Bearer " + GenerateMockToken(userId); // Add the token as a Bearer token
 
-        controller.ControllerContext = new ControllerContext()
+            controller.ControllerContext = new ControllerContext()
+            {
+                HttpContext = httpContext
+            };
+        }
+        public void SetupTestEnvironment(string userId, string leaderboardName)
         {
-            HttpContext = httpContext
-        };
-    }
-    public void SetupTestEnvironment(string userId, string leaderboardName)
-    {
-        // Arrange mock services
-        _leaderboardService.Setup(x => x.GetLeaderboardByNameAsync(leaderboardName)).ReturnsAsync(new Leaderboard());
-        _highScoreService.Setup(x => x.CheckIfItsHighScore(It.IsAny<HighScore>(), leaderboardName)).ReturnsAsync(true);
-        _userService.Setup(x => x.GetUserByIdAsync(userId)).ReturnsAsync(new User { UserId = userId });
-        _userService.Setup(x => x.UserExistsAsync(userId)).ReturnsAsync(true);
-        _leaderboardService.Setup(x => x.CheckIfStudentHasHighScoreInLeadeboard(userId, leaderboardName)).ReturnsAsync(false);
-        _leaderboardService.Setup(x => x.AddHighScoreAsync(It.IsAny<HighScore>(), leaderboardName)).ReturnsAsync(true);
-        _highScoreService.Setup(x => x.UpdateMainLeaderboard(userId)).ReturnsAsync(true);
+            // Arrange mock services
+            _leaderboardService.Setup(x => x.GetLeaderboardByNameAsync(leaderboardName)).ReturnsAsync(new Leaderboard());
+            _highScoreService.Setup(x => x.CheckIfItsHighScore(It.IsAny<HighScore>(), leaderboardName)).ReturnsAsync(true);
+            _userService.Setup(x => x.GetUserByIdAsync(userId)).ReturnsAsync(new User { UserId = userId });
+            _userService.Setup(x => x.UserExistsAsync(userId)).ReturnsAsync(true);
+            _leaderboardService.Setup(x => x.CheckIfStudentHasHighScoreInLeadeboard(userId, leaderboardName)).ReturnsAsync(false);
+            _leaderboardService.Setup(x => x.AddHighScoreAsync(It.IsAny<HighScore>(), leaderboardName)).ReturnsAsync(true);
+            _highScoreService.Setup(x => x.UpdateMainLeaderboard(userId)).ReturnsAsync(true);
 
-        // Arrange the User property of the controller with a ClaimsPrincipal that has the necessary claims
-        var claims = new List<Claim>
+            // Arrange the User property of the controller with a ClaimsPrincipal that has the necessary claims
+            var claims = new List<Claim>
     {
         new Claim(ClaimTypes.Name, userId)
     };
-        var identity = new ClaimsIdentity(claims);
-        var principal = new ClaimsPrincipal(identity);
-        _controller.ControllerContext = new ControllerContext
+            var identity = new ClaimsIdentity(claims);
+            var principal = new ClaimsPrincipal(identity);
+            _controller.ControllerContext = new ControllerContext
+            {
+                HttpContext = new DefaultHttpContext { User = principal }
+            };
+        }
+        [Fact]
+        public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenUserDoesNotExist()
         {
-            HttpContext = new DefaultHttpContext { User = principal }
-        };
-    }
-    [Fact]
-    public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenUserDoesNotExist()
-    {
-        // Arrange
-        SetupControllerUser(_controller, "NonexistentUser");
+            // Arrange
+            SetupControllerUser(_controller, "NonexistentUser");
 
-        // Act
-        var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
+            // Act
+            var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
 
-        // Assert
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-    [Fact]
-    public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenLeaderboardNameIsNull()
-    {
-        // Arrange
-        SetupTestEnvironment("ExistingUser", "Leaderboard1");
+        [Fact]
+        public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenLeaderboardNameIsNull()
+        {
+            // Arrange
+            SetupTestEnvironment("ExistingUser", "Leaderboard1");
 
-        // Act
-        var result = await _controller.AddHighScoreToLeaderboard(100, null);
+            // Act
+            var result = await _controller.AddHighScoreToLeaderboard(100, null);
 
-        // Assert
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-    [Fact]
-    public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenLeaderboardNameIsMain()
-    {
-        // Arrange
-        SetupTestEnvironment("ExistingUser", "Leaderboard1");
+        [Fact]
+        public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenLeaderboardNameIsMain()
+        {
+            // Arrange
+            SetupTestEnvironment("ExistingUser", "Leaderboard1");
 
-        // Act
-        var result = await _controller.AddHighScoreToLeaderboard(100, "Main");
+            // Act
+            var result = await _controller.AddHighScoreToLeaderboard(100, "Main");
 
-        // Assert
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-    [Fact]
-    public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenUpdateMainLeaderboardFails()
-    {
-        // Arrange
-        SetupTestEnvironment("ExistingUser", "Leaderboard1");
-        _highScoreService.Setup(x => x.UpdateMainLeaderboard(It.IsAny<string>())).ReturnsAsync(false);
+        [Fact]
+        public async Task AddHighScoreToLeaderboard_ShouldReturnBadRequest_WhenUpdateMainLeaderboardFails()
+        {
+            // Arrange
+            SetupTestEnvironment("ExistingUser", "Leaderboard1");
+            _highScoreService.Setup(x => x.UpdateMainLeaderboard(It.IsAny<string>())).ReturnsAsync(false);
 
-        // Act
-        var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
+            // Act
+            var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
 
-        // Assert
-        Assert.IsType<BadRequestObjectResult>(result);
-    }
+            // Assert
+            Assert.IsType<BadRequestObjectResult>(result);
+        }
 
-    [Fact]
-    public async Task AddHighScoreToLeaderboard_ShouldReturnOk_WhenHighScoreIsAddedAndMainLeaderboardIsUpdated()
-    {
-        // Arrange
-        SetupTestEnvironment("ExistingUser", "Leaderboard1");
-        _highScoreService.Setup(x => x.UpdateMainLeaderboard(It.IsAny<string>())).ReturnsAsync(true);
+        [Fact]
+        public async Task AddHighScoreToLeaderboard_ShouldReturnOk_WhenHighScoreIsAddedAndMainLeaderboardIsUpdated()
+        {
+            // Arrange
+            SetupTestEnvironment("ExistingUser", "Leaderboard1");
+            _highScoreService.Setup(x => x.UpdateMainLeaderboard(It.IsAny<string>())).ReturnsAsync(true);
 
-        // Act
-        var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
+            // Act
+            var result = await _controller.AddHighScoreToLeaderboard(100, "Leaderboard1");
 
-        Assert.Null(result);
-        // Assert
-        Assert.IsType<OkResult>(result);
+            Assert.Null(result);
+            // Assert
+            Assert.IsType<OkResult>(result);
+        }
     }
 }
